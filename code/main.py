d0ffb36d05e65b52452d110c14b08a5effe48a3d
kiyari005 <letuankiet005@gmail.com>
2024-10-03 12:32:48 +0700

    underworld!!

---
+++ code/main.py
@@ -0,0 +1,58 @@
+from settings import *
+from player import Player
+from sprites import *
+from pytmx import load_pygame
+
+from random import randint
+
+class Game:
+    def __init__(self):
+        #setup
+        pygame.init()
+        self.display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+        pygame.display.set_caption('underworld')
+        self.clock = pygame.time.Clock()
+        self.running = True
+        
+        #groups
+        self.all_sprites = pygame.sprite.Group()
+        self.collision_sprites = pygame.sprite.Group()
+        
+        self.setup()
+                        
+        #sprites
+        self.player = Player((400,300), self.all_sprites, self.collision_sprites)
+    
+    def setup(self):
+        map = load_pygame(join('data', 'maps', 'world.tmx'))
+        for obj in map.get_layer_by_name('Objects'):
+            CollisionSprite((obj.x, obj.y), obj.image, (self.all_sprites, self.collision_sprites))
+            print(obj.x)
+            print(obj.y)
+            print(obj.image)
+            
+    
+        
+    def run(self):
+        while self.running:
+            # dt
+            dt = self.clock.tick() / 1000
+        
+            # event loop
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.running = False
+
+            # update
+            self.all_sprites.update(dt)
+        
+            # draw
+            self.display_surface.fill('black')
+            self.all_sprites.draw(self.display_surface)
+            pygame.display.update()
+        
+        pygame.quit()
+        
+if __name__ == '__main__':        
+    game = Game()
+    game.run()
