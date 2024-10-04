467b681e625d7f3952a4881fc770e3eee68f9f63
kiyari005 <letuankiet005@gmail.com>
2024-10-05 00:29:10 +0700

    gun

--- code/main.py
+++ code/main.py
@@ -2,6 +2,7 @@ from settings import *
 from player import Player
 from sprites import *
 from pytmx.util_pygame import load_pygame
+from groups import AllSprites
 
 from random import randint
 
@@ -15,23 +16,28 @@ class Game:
         self.running = True
         
         #groups
-        self.all_sprites = pygame.sprite.Group()
+        self.all_sprites = AllSprites()
         self.collision_sprites = pygame.sprite.Group()
         
         self.setup()
                         
         #sprites
-        self.player = Player((400,300), self.all_sprites, self.collision_sprites)
-    
+        
     def setup(self):
         map = load_pygame(join('data', 'maps', 'world.tmx'))
-        for obj in map.get_layer_by_name('Objects'):
-            CollisionSprite((obj.x, obj.y), obj.image, (self.all_sprites, self.collision_sprites))
-            print(obj.x)
-            print(obj.y)
-            print(obj.image)
+        for x, y, image in map.get_layer_by_name('Ground').tiles():          
+            Sprite((x * TILE_SIZE, y * TILE_SIZE), image, self.all_sprites)     
             
-    
+        for obj in map.get_layer_by_name('Objects'):
+            CollisionSprite((obj.x, obj.y), obj.image, (self.all_sprites, self.collision_sprites))    
+            
+        for obj in map.get_layer_by_name('Collisions'):
+            CollisionSprite((obj.x, obj.y), pygame.Surface((obj.width, obj.height)), self.collision_sprites)
+            
+        for obj in map.get_layer_by_name('Entities'):
+            if obj.name == 'Player':
+                self.player = Player((obj.x, obj.y), self.all_sprites, self.collision_sprites)
+                self.gun = Gun(self.player, self.all_sprites)
         
     def run(self):
         while self.running:
@@ -48,7 +54,7 @@ class Game:
         
             # draw
             self.display_surface.fill('black')
-            self.all_sprites.draw(self.display_surface)
+            self.all_sprites.draw(self.player.rect.center)
             pygame.display.update()
         
         pygame.quit()
--- code/sprites.py
+++ code/sprites.py
@@ -5,6 +5,7 @@ class Sprite(pygame.sprite.Sprite):
         super().__init__(groups)
         self.image = surf
         self.rect = self.image.get_rect(topleft = pos)
+        self.ground = True
 
 class CollisionSprite(pygame.sprite.Sprite):
     def __init__(self, pos, surf, groups):
@@ -12,3 +13,26 @@ class CollisionSprite(pygame.sprite.Sprite):
         self.image = surf
         self.rect = self.image.get_rect(topleft = pos)
         
+class Gun(pygame.sprite.Sprite):
+    def __init__(self, player, groups):
+        #player connection
+        self.player = player
+        self.distance = 140
+        self.player_direction = pygame.Vector2(1,0)
+        
+        #sprite setup
+        super().__init__(groups)
+        self.gun_surf = pygame.image.load(join('images', 'gun', 'gun.png')).convert_alpha()
+        self.image = self.gun_surf
+        self.rect = self.image.get_rect(center = self.player.rect.center + self.player.direction * self.distance)
+        
+        
+    def get_direction(self):
+        mouse_pos = pygame.Vector2(pygame.mouse.get_pos())
+        player_pos = pygame.Vector2(WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2)
+        self.player_direction = (mouse_pos - player_pos).normalize()
+        print(self.player_direction)
+            
+    def update(self, _):
+        self.get_direction()
+        self.rect.center = self.player.rect.center + self.player.direction * self.distance
