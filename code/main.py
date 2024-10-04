1aadc79a4b896d14962219869a74cdd30c530dc9
kiyari005 <letuankiet005@gmail.com>
2024-10-05 01:37:29 +0700

    gun

--- code/main.py
+++ code/main.py
@@ -14,15 +14,36 @@ class Game:
         pygame.display.set_caption('underworld')
         self.clock = pygame.time.Clock()
         self.running = True
+        self.load_images()
         
         #groups
         self.all_sprites = AllSprites()
         self.collision_sprites = pygame.sprite.Group()
+        self.bullet_sprites = pygame.sprite.Group()
         
         self.setup()
                         
-        #sprites
+        #gun timer
+        self.can_shoot = True
+        self.shoot_time = 0
+        self.gun_cooldown = 600
+    
+    def load_images(self):
+        self.bullet_surf = pygame.image.load(join('images', 'gun', 'bullet.png')).convert_alpha()
         
+    def input(self):
+        if pygame.mouse.get_pressed()[0] and self.can_shoot:
+            pos = self.gun.rect.center + self.gun.player_direction * 50
+            Bullet(self.bullet_surf, pos, self.gun.player_direction, (self.all_sprites, self.bullet_sprites))
+            self.can_shoot = False
+            self.shoot_time = pygame.time.get_ticks()
+            
+    def gun_timer(self):
+        if not self.can_shoot:
+            current_time = pygame.time.get_ticks()
+            if current_time - self.shoot_time >= self.gun_cooldown:        
+                self.can_shoot = True
+            
     def setup(self):
         map = load_pygame(join('data', 'maps', 'world.tmx'))
         for x, y, image in map.get_layer_by_name('Ground').tiles():          
@@ -50,12 +71,15 @@ class Game:
                     self.running = False
 
             # update
+            self.gun_timer()
+            self.input()
             self.all_sprites.update(dt)
         
             # draw
             self.display_surface.fill('black')
             self.all_sprites.draw(self.player.rect.center)
             pygame.display.update()
+            print(self.bullet_sprites)
         
         pygame.quit()
         
